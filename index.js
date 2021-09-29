1523c295cbf5ecd88dc59b9cebdfb130e9d754d3
Max Gabriel <90213150+fubuki-9@users.noreply.github.com>
2021-09-29 16:34:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const ongakuchan = require("./structures/ongakuchan");
+const ongakuchan = require("./structures/ongakuchan");
+const client = new ongakuchan();
+
+client.build();
+
+module.exports = client; //;-;
